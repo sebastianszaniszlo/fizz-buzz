01d0b6efd4058461a5d6fb35c7fb7263903574dd
Seb <sebastian.szaniszlo@gmail.com>
2024-02-19 21:22:42 +0000

    Translator tests. FizzBuzz scenario is failing

---
+++ FizzBuzz.UnitTests/Translator/TranslatorTests.cs
@@ -0,0 +1,47 @@
+﻿using FizzBuzz.Services;
+using FluentAssertions;
+
+namespace FizzBuzz.UnitTests.Translator
+{
+    public class TranslatorTests
+    {
+        [Fact]
+        public void WhenInvokedWithNumberDivisibleByThreeShouldReturnFizz()
+        {
+            var translator = new FizzBuzzTranslator();
+
+            string result = "";
+            Action act = () => result = translator.Translate(9);
+
+            act.Should().NotThrow();
+            result.Should().Be("Fizz");
+            
+        }
+
+        [Fact]
+        public void WhenInvokedWithNumberDivisibleByFiveShouldReturnBuzz()
+        {
+            var translator = new FizzBuzzTranslator();
+
+            string result = "";
+            Action act = () => result = translator.Translate(25);
+
+            act.Should().NotThrow();
+            result.Should().Be("Buzz");
+
+        }
+
+        [Fact]
+        public void WhenInvokedWithNumberDivisibleByThreeAndFiveShouldReturnFizzBuzz()
+        {
+            var translator = new FizzBuzzTranslator();
+
+            string result = "";
+            Action act = () => result = translator.Translate(15);
+
+            act.Should().NotThrow();
+            result.Should().Be("FizzBuzz");
+
+        }
+    }
+}
